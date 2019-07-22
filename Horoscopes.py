8afcc58e4343d1308732a0e241737a40ad41c5c2
tsrn37992307 <39452933+tsrn37992307@users.noreply.github.com>
2019-07-23 01:40:43 +0800

    Horoscopes0723

--- Horoscopes.py
+++ Horoscopes.py
@@ -1,15 +1,15 @@
 import requests
 import pymysql
 import os,re,time
+import datetime
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-con = pymysql.connect(host = '127.0.0.1',port = 3307 , user = "root" ,passwd = "",db = "horoscopes")
+con = pymysql.connect(host = '127.0.0.1',port = 3306 , user = "root" ,passwd = "",db = "horoscopes",charset = 'utf8')
 cur = con.cursor()
 
+
 url = "https://www.dcard.tw/f/horoscopes?latest=true"
-#res = requests.get(url)
-#soup = BeautifulSoup(res.text,'html.parser')
 page = 0
 number = 1
 newgender = []
@@ -18,13 +18,18 @@ newgender = []
 mypath = os.getcwd()
 #開啟ChromeDriver
 driver =  webdriver.Chrome(mypath+'\\chromedriver')
+
 #等待3秒
 driver.implicitly_wait(3)
+
 driver.get(url)
+driver.find_element_by_class_name('Header_navText_1m5321').click()
 Height = driver.execute_script('return document.body.scrollHeight')
 
+#資料庫新增指令
+sql = "INSERT INTO horoscopes (school,title,gender,DateTime) VALUE (%s,%s,%s,%s)"
 
-for i in range (1,3):
+for i in range (1,99):
     okay = 0
     page+=1
     #javascript指令，使網頁下拉至最底層
@@ -39,7 +44,7 @@ for i in range (1,3):
 
     for h in range(len(DateTime)):
         print("第" ,h, "筆 :" , DateTime[h].text)
-        if DateTime[h].text[:5] == "7月21日":
+        if DateTime[h].text[:5] == "7月19日":
             okay = 1
             print("已抓完今日到20日的所有文章")
             break
@@ -47,13 +52,9 @@ for i in range (1,3):
         break
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
-#學校、標題、性別
 school = soup.find_all('span',re.compile('PostAuthor_root'))
-#print(school)
 title = soup.find_all('h3',re.compile('Title__Text-v196i6-0'))
-#print(title)
 gender = soup.find_all('div',re.compile('AnonymousAvatar'))
-#print(gender)
 
 for i in range(len(gender)):
     #print("gender : " , gender[i])
@@ -68,17 +69,13 @@ for i in range(len(gender)):
 try:
     for count in range (0,999):
         print("第" + str(number) + "筆 : ","\n學校 : " +str(school[count].text), "\n標題 : " + str(title[count].text ), "\n性別 : " + str(newgender[count]),"\n時間 : " + str(DateTime[count].text))
-        #sql = "INSERT INTO dcard(school,title,gender,DateTime)VALUES ('%s','%s','%s','%s',)" % (str(school[coumt].text),str(title[count].text),str(newgender[count]),str(DateTime[coumt].text))
-        #cur.execute(sql)
-        # Commit your changes in the database
-        #con.commit()
+        data = [(school[count].text, title[count].text, newgender[count], DateTime[count].text)]
+        cur.executemany(sql,data)
+        con.commit()
         number+=1
 except:
     print("\n抓取完成")
-    sql = "INSERT INTO dcard(school,title,gender,DateTime)VALUES ('%s','%s','%s','%s',)" % (str(school[1].text), str(title[1].text), str(newgender[1]), str(DateTime[1].text))
-    cur.execute(sql)
-    # Commit your changes in the database
-    con.commit()
     con.close()
+
 
 
